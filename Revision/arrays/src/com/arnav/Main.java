2c48558f1699a3af9f4b58bb60ebb473e79c5207
Arnav Barman <arnavbarman1@gmail.com>
2022-03-16 03:54:26 +0530

    more revision

--- Revision/arrays/src/com/arnav/Main.java
+++ Revision/arrays/src/com/arnav/Main.java
@@ -1,8 +1,19 @@
 package com.arnav;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[3][2];
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = in.nextInt();
+            }
+        }
+        for (int[] x : arr) {
+            System.out.println(Arrays.toString(x));
+        }
     }
 }
