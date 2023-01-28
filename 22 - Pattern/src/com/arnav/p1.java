05c35e7a78db8366a19af565d790d2a187c2f57f
Arnav Barman <arnavbarman1@gmail.com>
2023-01-29 05:10:43 +0530

    Added Pattern

---
+++ 22 - Pattern/src/com/arnav/p1.java
@@ -0,0 +1,9 @@
+package com.arnav;
+public class p1 {
+    public static void main(String[] args){
+        int n =5;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*****");
+        }
+    }
+}
