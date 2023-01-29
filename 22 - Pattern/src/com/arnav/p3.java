0a9c335a70c48f49cec39be52459bcd2a9e73703
Arnav Barman <arnavbarman1@gmail.com>
2023-01-29 05:30:36 +0530

    Added p4

--- 22 - Pattern/src/com/arnav/p3.java
+++ 22 - Pattern/src/com/arnav/p3.java
@@ -2,9 +2,8 @@ package com.arnav;
 
 public class p3 {
     public static void main(String[] args) {
-        int n=5;
-        for (int i = 0; i < 5; i++) {
-            for (int j = n-i; j>0 ; j--) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = i; j>0 ; j--) {
                 System.out.print("*");
             }
             System.out.println("");
